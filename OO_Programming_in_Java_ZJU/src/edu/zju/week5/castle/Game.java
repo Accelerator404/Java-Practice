0578c955a4571a8652e04f660a6db9aeae12fd5a
Yuudach1 <adsl2328606@gmail.com>
2020-01-08 12:05:23 +0800

    For oj homework.

--- OO_Programming_in_Java_ZJU/src/edu/zju/week5/castle/Game.java
+++ OO_Programming_in_Java_ZJU/src/edu/zju/week5/castle/Game.java
@@ -4,30 +4,32 @@ import java.util.Scanner;
 
 public class Game {
     private Room currentRoom;
-        
-    public Game() 
-    {
+
+    public Game() {
         createRooms();
     }
 
-    private void createRooms()
-    {
+    private void createRooms() {
         Room outside, lobby, pub, study, bedroom;
-      
+
         //	制造房间
         outside = new Room("城堡外");
         lobby = new Room("大堂");
         pub = new Room("小酒吧");
         study = new Room("书房");
         bedroom = new Room("卧室");
-        
-        //	初始化房间的出口
-        outside.setExits(null, lobby, study, pub);
-        lobby.setExits(null, null, null, outside);
-        pub.setExits(null, outside, null, null);
-        study.setExits(outside, bedroom, null, null);
-        bedroom.setExits(null, null, null, study);
 
+        //	初始化房间的出口
+        outside.setExit("east", lobby);
+        outside.setExit("south", study);
+        outside.setExit("west", pub);
+        lobby.setExit("west", outside);
+        pub.setExit("east", outside);
+        study.setExit("north", outside);
+        study.setExit("east", bedroom);
+        bedroom.setExit("west", study);
+        lobby.setExit("up",pub);
+        pub.setExit("down",lobby);
         currentRoom = outside;  //	从城堡门外开始
     }
 
@@ -42,50 +44,47 @@ public class Game {
 
     // 以下为用户命令
 
-    private void printHelp() 
-    {
+    private void printHelp() {
         System.out.print("迷路了吗？你可以做的命令有：go bye help");
         System.out.println("如：\tgo east");
     }
 
-    private void goRoom(String direction) 
-    {
+    private void goRoom(String direction) {
         Room nextRoom = currentRoom.getExit(direction);
         if (nextRoom == null) {
             System.out.println("那里没有门！");
-        }
-        else {
+        } else {
             currentRoom = nextRoom;
             showPrompt();
         }
     }
 
-    public void showPrompt(){
+    public void showPrompt() {
         System.out.println("你在" + currentRoom);
         System.out.print("出口有: ");
         System.out.print(currentRoom.getExitDesc());
         System.out.println();
     }
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		Game game = new Game();
-		game.printWelcome();
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Game game = new Game();
+        game.printWelcome();
 
-        while ( true ) {
-        		String line = in.nextLine();
-        		String[] words = line.split(" ");
-        		if ( words[0].equals("help") ) {
-        			game.printHelp();
-        		} else if (words[0].equals("go") ) {
-        			game.goRoom(words[1]);
-        		} else if ( words[0].equals("bye") ) {
-        			break;
-        		}
+        while (true) {
+            String line = in.nextLine();
+            String[] words = line.split(" ");
+            if (words[0].equals("help")) {
+                game.printHelp();
+            } else if (words[0].equals("go")) {
+                game.goRoom(words[1]);
+            } else if (words[0].equals("bye")) {
+                break;
+            }
         }
-        
+
         System.out.println("感谢您的光临。再见！");
         in.close();
-	}
+    }
 
 }
